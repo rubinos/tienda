287b7161e784dedab3c50bba90c5d1056ccb8f67
manuelamadeo <manuelbuendia26@gmail.com>
2016-02-23 21:41:32 +0100

    dsgfs

--- s3-us-west-2.amazonaws.com/imagenes.paginas.infomovil/Templates/Clasico/js/functions.js
+++ s3-us-west-2.amazonaws.com/imagenes.paginas.infomovil/Templates/Clasico/js/functions.js
@@ -1,15 +1,1 @@
-  //  Esta funcion permite insertar los conteos  de los anuncios publicados
-function setCountAnuncio(idPublicidad, idDominio)
-{	
-	$.ajax({  
-	    type: "GET",  
-	    url:"contadorPublicidad.action?idAnuncio="+idPublicidad+"&idDominio="+idDominio, 
-	    success: function(response){  
-	    },  
-	    error: function(e){  
-	        
-	    },
-	    async: true, 
-	    cache: false  
-	});  
-}  
+function setCountAnuncio(idPublicidad,idDominio){$.ajax({type:"GET",url:"contadorPublicidad.action?idAnuncio="+idPublicidad+"&idDominio="+idDominio,success:function(response){},error:function(e){},async:true,cache:false});}

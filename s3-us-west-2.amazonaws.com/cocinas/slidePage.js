868f9ea7509c8fb11d554eb393e0fdf1a26e3269
manuelamadeo <manuelbuendia26@gmail.com>
2016-02-25 15:49:59 +0100

    ss

---
+++ s3-us-west-2.amazonaws.com/cocinas/slidePage.js
@@ -0,0 +1,132 @@
+var extraTextlines = 0;
+var shadowWidthSlide= 0;
+slidePadding = 0;
+var orgFontsize= 16;
+var orgh1Fontsize= 22;
+var orgbuttonFontsize= 28;
+
+var buttonOpacity= '1';
+var ClickF11 = "Pulse F11 para ver imágenes más grandes, haz click en una foto.";
+var fsMAC = "Press Command+Control+F to view this album in Full screen";
+
+var hideIndexPage = false;
+
+
+var links_under_thumbs = false;
+
+
+var enableTaps = false;
+
+
+var limitLongDescriptions = false;
+
+var useBrowserVideo = false;
+var stopAfterLastSlide = false;
+var commentType = 1;
+var borderWidth = 2;
+
+var videoWidth = 0;
+var imgThemeMinScreenHeight = '400';
+var folderItemMinScreenHeight = '400';
+var swipesMinScreenHeight = '400';
+var swipesArea = 0;
+
+var imageBorderColor = '#ffffffff';
+var thumbmovieBorderColor = 'rgba(254,0,55,1.0)';
+viewtime = 1000;
+transeffect = 0;
+transtime = 0;
+hidenav = 1;
+
+var allowSharing = false;
+hideshare = 1;
+enableOversizedImage = 0;
+pause = false;
+
+var albumImageName = 'SDC14986.jpg';
+var albumImageIndex = 0;
+
+var srcImg = [
+'slides/SDC14986.jpg',
+'slides/SDC15321.jpg',
+'slides/20160218_161714.jpg',
+'slides/fotos%20cortinas%20023.jpg',
+'slides/mayo%202012%20030.jpg',
+'slides/SDC12346.jpg',
+'slides/SDC14376.jpg',
+'slides/SDC14753.jpg',
+'slides/SDC14976.jpg',
+'slides/SDC14978.jpg',
+];
+
+var srcHiResImg = [
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+];
+
+var wImg = [
+810,
+810,
+810,
+810,
+1440,
+810,
+810,
+810,
+810,
+810,
+];
+var hImg = [
+1080,
+1080,
+1080,
+1080,
+1080,
+1080,
+1080,
+1080,
+1080,
+1080,
+];
+var strCap = [
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+];
+
+
+
+ var strTitle = [
+ 'SDC14986',
+ 'SDC15321',
+ '20160218_161714',
+ 'fotos cortinas 023',
+ 'mayo 2012 030',
+ 'SDC12346',
+ 'SDC14376',
+ 'SDC14753',
+ 'SDC14976',
+ 'SDC14978',
+ ];
+
+
+
+
+
+
+
